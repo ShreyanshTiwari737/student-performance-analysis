bd784692e79511bec7960b29d3dfba9e09e2b73f
ShreyanshTiwari737 <shreyanshtiwari9520@gmail.com>
2026-04-25 00:01:25 +0530

    Implement data preprocessing functions in preprocess.py
    Added functions for data loading, handling missing values, removing outliers, and feature engineering.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,24 @@
-
+import pandas as pd
+import numpy as np
+def load_data(path):
+    df = pd.read_csv(path)
+    print("Dataset loaded. Shape:", df.shape)
+    print(df.head())
+    return df
+def handle_missing(df):
+    df['Marks'] = df['Marks'].fillna(df['Marks'].mean())
+    df['StudyHours'] = df['StudyHours'].fillna(df['StudyHours'].median())
+    print("Missing values handled.")
+    return df
+def remove_outliers(df):
+    df=df[df['StudyHours'] <= 15]
+    df=df[df['Marks'] <= 100]
+    print("Outliers removed. Shape:", df.shape)
+    return df
+def feature_engineering(df):
+    conditions= [df['Marks'] >= 80, df['Marks'] >= 60]
+    choices= ['Excellent', 'Good']
+    df['Performance']= np.select(conditions, choices, default='Needs Improvement')
+    df['EffortScore']= df['StudyHours'] * df['Attendance']
+    print("Features created: Performance, EffortScore")
+    return df
